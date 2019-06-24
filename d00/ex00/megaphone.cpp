a313c0971714b368e77a51acad00465e073b118b
Iipal <malkoleyplay@gmail.com>
2019-06-24 08:38:30 +0300

    added d00/ex00

---
+++ d00/ex00/megaphone.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstddef>
+
+static char *__strupr(char *src) {
+    size_t  i = ~0ULL;
+
+    while (src[++i])
+        src[i] = toupper(src[i]);
+    return (src);
+}
+
+int main(int ac, char *av[]) {
+    --ac; ++av;
+
+    if (!ac)
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+    else {
+        int i = -1;
+
+        while (ac > ++i)
+            std::cout << __strupr(av[i]);
+        std::cout << std::endl;
+    }
+}
