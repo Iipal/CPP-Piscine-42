5b41c4d9a65265a4b63f2792ab37e82387a8b7b2
Iipal <malkoleyplay@gmail.com>
2019-06-25 16:19:27 +0300

    ex05 stars

---
+++ d01/ex05/Brain.cpp
@@ -0,0 +1,8 @@
+#include "Brain.hpp"
+
+Brain::Brain(){}
+Brain::~Brain(){}
+
+std::string Brain::indetify(void) const {
+
+}
---
+++ d01/ex05/Brain.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <iostream>
+
+struct  Brain{
+    Brain();
+    ~Brain();
+
+    std::string indetify(void) const;
+};
