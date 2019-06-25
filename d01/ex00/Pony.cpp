1ec5c87cdb299972107d42c73ffd8879c292e21e
Iipal <malkoleyplay@gmail.com>
2019-06-25 10:56:45 +0300

    d01/ex00

--- d01/ex00/Pony.cpp
+++ d01/ex00/Pony.cpp
@@ -4,7 +4,7 @@ Pony::Pony() {}
 
 Pony::Pony(std::string ponyName) {
     this->_name = ponyName;
-    std::cout << "Congrats, new pony in stack was created with name: " << this->_name << std::endl
+    std::cout << "Congrats, new pony was born, she name is: " << this->_name << std::endl
         << " ," << std::endl
         << "/,`\\" << std::endl
         << "` | \\____\\\\" << std::endl
@@ -12,7 +12,16 @@ Pony::Pony(std::string ponyName) {
         << " \\-\\~~~_|\\  \\" << std::endl
         << "    \\ `   \\  `" << std::endl
         << "    `     `" << std::endl
-        << "Such a good name for this little pretty *-*" << std::endl;
+        << "Ah, " << this->_name << ", such a good name for this little pretty *-*" << std::endl;
 }
 
-Pony::~Pony() {}
+Pony::~Pony() {
+    std::cout << "Oh no, this little pretty " << this->_name << " run away from u ... " << std::endl
+        << ",~~_" << std::endl
+        << "|/\\ =_ _ ~" << std::endl
+        << " _( )_( )\\~~" << std::endl
+        << " \\,\\  _|\\ \\~~~" << std::endl
+        << "    \\`   \\" << std::endl
+        << "    `    `" << std::endl
+        << "What you done with her, bastard ... ? T.T" << std::endl;
+}
--- d01/ex00/main.cpp
+++ d01/ex00/main.cpp
@@ -1,5 +1,18 @@
 #include "Pony.hpp"
 
+Pony    ponyOnTheStack(void) { return (Pony("Leyla")); }
+Pony    *ponyOnTheHeap(void) { return (new Pony("Millie")); }
+
 int main(void) {
-    Pony p("pidor");
+    std::cout << "\tCreating pony on the stack: " << std::endl;
+    Pony    __sPony = ponyOnTheStack();
+
+
+    std::cout << "\n\n\tCreating pony on the heap: " << std::endl;
+    Pony    *__hPony = ponyOnTheHeap();
+
+    std::cout << "\n\n\tDeleting pony on the heap: " << std::endl;
+    delete __hPony;
+
+    std::cout << "\n\n\tDeleting pony on the stack: " << std::endl;
 }
