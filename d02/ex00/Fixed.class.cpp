3da4f2d4097266725ad6d12928def2b66e591bdf
Iipal <malkoleyplay@gmail.com>
2019-06-26 14:16:13 +0300

    d02/ex00

---
+++ d02/ex00/Fixed.class.cpp
@@ -0,0 +1,32 @@
+#include "Fixed.class.hpp"
+
+const int Fixed::_bits = 8;
+
+Fixed::Fixed() : _value(0) {
+    std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::Fixed(Fixed const &src) {
+    std::cout << "Copy constructor called" << std::endl;
+    *this = src;
+}
+
+Fixed::~Fixed() {
+    std::cout << "Destructor called" << std::endl;
+}
+
+Fixed   &Fixed::operator=(Fixed const &a) {
+    std::cout << "Assignation operator called" << std::endl;
+    if (this != &a)
+        this->setRawBits(a.getRawBits());
+    return *this;
+}
+
+int     Fixed::getRawBits(void) const {
+    std::cout << "getRawBits member function called" << std::endl;
+    return this->_value;
+}
+
+void    Fixed::setRawBits(int const raw) {
+    this->_value = raw;
+}
---
+++ d02/ex00/Fixed.class.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+
+class   Fixed {
+public:
+    Fixed();
+    Fixed(Fixed const &src);
+    ~Fixed();
+
+    Fixed   &operator=(Fixed const &a);
+
+    int     getRawBits(void) const;
+    void    setRawBits(int const raw);
+private:
+    int _value;
+    static const int _bits;
+};
