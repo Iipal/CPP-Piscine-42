c15f75b9185a8849885bb5fc835038c4ecf0f404
Iipal <malkoleyplay@gmail.com>
2019-06-29 15:09:59 +0300

    d04/ex03

---
+++ d04/ex03/Cure.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "AMateria.hpp"
+
+class Cure : public AMateria {
+public:
+    Cure();
+    Cure(Cure const &copy);
+    virtual ~Cure();
+
+    Cure &operator=(const Cure &copy);
+
+    AMateria *clone(void) const;
+    void use(ICharacter &target);
+};
---
+++ d04/ex03/MateriaSource.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Character.hpp"
+#include "IMateriaSource.hpp"
+
+class MateriaSource : public IMateriaSource {
+public:
+    MateriaSource();
+    MateriaSource(MateriaSource const &copy);
+    ~MateriaSource();
+
+    MateriaSource &operator=(const MateriaSource &copy);
+
+    void learnMateria(AMateria*);
+    AMateria *createMateria(std::string const &type);
+private:
+    int currentlyStoredMaterials_;
+    AMateria *store_[MAX_SLOTS];
+};
