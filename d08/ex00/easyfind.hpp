7953204a86181763468db6919a110832fe9659ba
Iipal <malkoleyplay@gmail.com>
2019-07-05 15:37:45 +0300

    find update (d07/ex02)

--- d08/ex00/easyfind.hpp
+++ d08/ex00/easyfind.hpp
@@ -1,15 +1,8 @@
 #pragma once
 
+#include <algorithm>
+
 template <typename T>
 typename T::iterator easyfind(T &container, int find) {
-    typename T::iterator it = container.begin();
-    typename T::iterator ite = container.end();
-
-    while (ite != it) {
-        if (*it == find) {
-            return it;
-        }
-        ++it;
-    }
-    return ite;
+    return std::find(container.begin(), container.end(), find);
 }
